6dd0592bb8426b21dcf94d55baceb360d6ce2c23
hyunje2316 <hyunje2316@gmail.com>
2021-01-03 23:17:55 +0900

    add project

---
+++ src/main/java/com/talmo/vim/GameVimApplication.java
@@ -0,0 +1,13 @@
+package com.talmo.vim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameVimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameVimApplication.class, args);
+	}
+
+}
