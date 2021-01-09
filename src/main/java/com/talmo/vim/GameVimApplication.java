609a6f6003802991a6ba768eeacda6d852f87219
홍성혁 <markhong93@naver.com>
2021-01-09 23:53:29 +0900

    properties ignore 처리

--- src/main/java/com/talmo/vim/GameVimApplication.java
+++ src/main/java/com/talmo/vim/GameVimApplication.java
@@ -2,12 +2,13 @@ package com.talmo.vim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GameVimApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GameVimApplication.class, args);
-	}
+   public static void main(String[] args) {
+      SpringApplication.run(GameVimApplication.class, args);
+   }
 
 }
